1a108d525055dd52bfb979cc99beb7c9ef6ceb5d
hafeez12qbyte <hafeez345u@gmail.com>
2026-05-10 19:03:08 +0500

    summary

--- myTravel/src/components/Navbar.jsx
+++ myTravel/src/components/Navbar.jsx
@@ -1,32 +1,33 @@
 
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 export default function Navbar() {
+  const navigate = useNavigate
   return (
-    <div>
+    <>
       <header>
         <div className="logo">
-            <h2>TripVista</h2>
-        </div>
-        <div className="nav">
-            <ul>
-                <li>
-                    <NavLink to="/">Home</NavLink>
-                </li>
-                 <li>
-                    <NavLink to="/destination">Destinations</NavLink>
-                </li>
-                 <li>
-                    <NavLink to="/packages">Packages</NavLink>
-                </li>
-                 <li>
-                    <NavLink to="/about">About</NavLink>
-                </li>
-                 <li>
-                    <NavLink to="/contact">Contact</NavLink>
-                </li>
-            </ul>
-        </div>
+          <h2 onClick={() => navigate("/")}>TripVista</h2>
+      </div>
+      <div className="nav">
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/destination">Destinations</NavLink>
+          </li>
+          <li>
+            <NavLink to="/packages">Packages</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+      </div>
       </header>
-    </div>
+    </>
   )
 }
