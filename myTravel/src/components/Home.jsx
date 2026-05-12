5e70f7f3b5f247502406b5ee56418f9c4bcda363
hafeez12qbyte <hafeez345u@gmail.com>
2026-05-12 15:35:44 +0500

    css changed

--- myTravel/src/components/Home.jsx
+++ myTravel/src/components/Home.jsx
@@ -1,9 +1,24 @@
 import 'react'
-
+// import Image from "../assets/Hero"
 export default function Home() {
   return (
     <div>
-      Home
+      <div className="sections">
+      <section className="hero">
+        <div className="hero-content">
+        <h1>This is the Hero Section.</h1>
+        <h3>Hero Section is the main section of a Website.</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum non, autem explicabo quo, minus omnis tenetur illum accusantium consequuntur similique recusandae? Cum quo odio aliquam placeat iste a repellat esse quos, quia quasi id, corporis omnis unde ad hic rem ex laboriosam optio nam numquam.
+        </p>
+        </div>
+        <div className="hero-img">
+           {/* <img src={Image} alt="Image not found" /> */}
+        <h1>Image Not Found</h1>
+      
+        </div>
+      </section>
+      </div>
     </div>
   )
 }
